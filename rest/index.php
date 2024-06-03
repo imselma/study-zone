51d3b971da5e5d06d666c6b78ccbacf18de14692
Selma Imširović <selma.imsirovic@stu.ibu.edu.ba>
2024-06-03 18:54:20 +0200

    edit vendor

--- rest/index.php
+++ rest/index.php
@@ -1,5 +1,5 @@
 <?php
-require '../vendor/autoload.php'; 
+require './vendor/autoload.php'; 
 require "./services/BaseService.php";
 require "./services/UserService.php";
 require "./services/TipsService.php";
